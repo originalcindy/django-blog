b2c7e5f639f11b3f6c8e1aa90a44b6f9f9288239
Charlene Campbell-Gray <illest.reps@gmail.com>
2024-12-01 16:46:12 +0000

    adds a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x(iprp1s^k$t_zz9!@2tg1pkd!nd=s&a5su+m!dl4r36s$36m*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-originalcind-djangoblog-oab8yjys0st.ws.codeinstitute-ide.net",".herokuapp.com"
 ]
